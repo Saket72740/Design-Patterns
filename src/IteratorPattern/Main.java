e89c0d2cd8969ac00fdcba3d717e07fb7515eddf
Saket Kumar <saket12345@yahoo.com>
2024-08-05 14:21:08 +0530

    Iterator pattern is ready

---
+++ src/IteratorPattern/Main.java
@@ -0,0 +1,24 @@
+package IteratorPattern;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Book> booksList = Arrays.asList(
+				new Book(100, "Science"),
+				new Book(200, "Maths"),
+				new Book(300, "GK"),
+				new Book(400, "Drawing")
+				);
+		
+		Library lib = new Library(booksList);
+		Iterator iterator = lib.createIterator();
+		
+		while(iterator.hasNext()) {
+			Book book =  (Book) iterator.next();
+			System.out.println(book.getBookName() + " book having price " + book.getPrice());
+		}
+	}
+}
