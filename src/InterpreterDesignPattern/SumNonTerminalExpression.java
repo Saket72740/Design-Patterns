97cb356c2d97df301c4354174d31848541eeaeaf
Saket Kumar <saket12345@yahoo.com>
2024-08-14 23:38:03 +0530

    Interpreter Design Pattern is ready

---
+++ src/InterpreterDesignPattern/SumNonTerminalExpression.java
@@ -0,0 +1,21 @@
+package InterpreterDesignPattern;
+
+public class SumNonTerminalExpression implements AbstractExpression{
+
+	AbstractExpression leftExpression;
+	AbstractExpression rightExpression;
+	
+	public SumNonTerminalExpression(AbstractExpression leftExpression,
+			AbstractExpression rightExpression) {
+		this.leftExpression = leftExpression;
+		this.rightExpression = rightExpression;
+	}
+
+	@Override
+	public int interpret(Context context) {
+		return leftExpression.interpret(context) + rightExpression.interpret(context);
+	}
+	
+	
+
+}
